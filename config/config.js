39f3e13dc43ce84a68e564ec0f0ac4acdf73c729
Jigme-Lhuendrup <07230029.sherubtse@rub.edu.bt>
2025-06-05 22:58:13 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,18 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // for Neon PostgreSQL
-    },
-  },
+  protocol: 'postgres',
+  dialectOptions: process.env.DATABASE_URL.includes("sslmode=require")
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      }
+    : {},
+  logging: false,
 });
 
-module.exports = sequelize;
+module.exports = { sequelize };
