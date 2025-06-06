9104e01e9da7b120dcd8c601e96ebab254703370
Jigme-Lhuendrup <07230029.sherubtse@rub.edu.bt>
2025-06-06 13:38:46 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,15 +4,19 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: process.env.DATABASE_URL.includes("sslmode=require")
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        }
-      }
-    : {},
-  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  logging: false
 });
 
 module.exports = { sequelize };
